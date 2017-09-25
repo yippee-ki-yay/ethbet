d2f543ec971e5cb91fb45dddb5f31de204dbe9eb
nenad <nesa993@gmail.com>
2017-09-25 15:17:46 +0200

    Adding the truffle boilerplate and ethbet contracts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Ethbet = artifacts.require("./Ethbet.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Ethbet);
+};
---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import EthbetContract from '../build/contracts/Ethbet.json'
+import getWeb3 from './utils/getWeb3'
+
+import './css/oswald.css'
+import './css/open-sans.css'
+import './css/pure-min.css'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      storageValue: 0,
+      web3: null
+    }
+  }
+
+  componentWillMount() {
+    // Get network provider and web3 instance.
+    // See utils/getWeb3 for more info.
+
+    getWeb3
+    .then(results => {
+      this.setState({
+        web3: results.web3
+      })
+
+      // Instantiate contract once web3 provided.
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+
+    const contract = require('truffle-contract');
+    const ethbetContract = contract(EthbetContract);
+    ethbetContract.setProvider(this.state.web3.currentProvider);
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar pure-menu pure-menu-horizontal">
+            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+        </nav>
+
+        <main className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              <h1>Good to Go!</h1>
+              <p>Your Truffle Box is installed and ready.</p>
+              <h2>Smart Contract Example</h2>
+              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+              <p>The stored value is: {this.state.storageValue}</p>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App
